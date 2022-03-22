1eda4f3631fbd1330638f79f70b56e18e828b4c1
iChuangCoder <94782292+iChuangCoder@users.noreply.github.com>
2022-03-22 13:49:35 +0800

    edit firstPicture format limit.

--- blog-view/src/admin/BlogEdit.vue
+++ blog-view/src/admin/BlogEdit.vue
@@ -132,16 +132,16 @@ export default {
       this.ruleForm.firstPicture = "https://cdn.ichuang.xyz/"+res.data
     },
     beforeAvatarUpload(file) {
-      const isJPG = file.type === 'image/jpeg';
+      // const isJPG = file.type === 'image/jpeg';
       const isLt2M = file.size / 1024 / 1024 < 2;
+      // if (!isJPG) {
+      //   this.$message.error('上传头像图片只能是 JPG 格式!');
+      // }
 
-      if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!');
-      }
       if (!isLt2M) {
         this.$message.error('上传头像图片大小不能超过 2MB!');
       }
-      return isJPG && isLt2M;
+      return isLt2M;
     },
 
     //双击将正文的内容前100字自动写入文章描述中
